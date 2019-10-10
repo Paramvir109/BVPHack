24cd832893dd5bd807a984b1525622e5f5058758
Manish Sahani <manishsahani1351111@gmail.com>
2019-10-10 17:33:09 +0530

    User Set

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.Promise  = global.Promise;//To tell mongoose we'll be using our inbuilt promise library
+
+mongoose.Promise  = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/HackBVP', {useNewUrlParser : true})
-//QuoraApp will be the name of database
 
 module.exports.mongoose = mongoose;
