f508b8b8ab5c923038d0711d1ef63a339a9b6d52
Manish Sahani <manishsahani1351111@gmail.com>
2019-10-10 22:37:01 +0530

    Form Added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,6 +7,11 @@ module.exports = {
     },
 
     callForService: async (params, callback) => {
+        // crate a request in db
+
+        // send this request to vendors 
+
+        // return a list of vendors 
         let list = await Vendor.getListByPlace(params.place);
         console.log(list);
         callback(list)
