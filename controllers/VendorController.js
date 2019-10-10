8295800cf45e555640e2be7ed9c5d0fa3088e34b
Manish Sahani <manishsahani1351111@gmail.com>
2019-10-10 16:52:52 +0530

    ejs set

---
+++ controllers/VendorController.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    index: (res, req) => {
+        
+    },
+
+
+}
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 app.use('/', require('./routes'));
+app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000
+
 app.listen(port, () => {
     console.log(`Server is up and running on ${port}`)
 })
