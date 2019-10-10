6f1c6158cb83889226eecbaa97417b83dce957f8
Paramvir Singh <paramvir109@gmail.com>
2019-10-10 17:20:26 +0530

    DB setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const {mongoose} = require('./mongoose')
+
 
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000
+
 
 app.listen(port, () => {
     console.log(`Server is up and running on ${port}`)
--- routes.js
+++ routes.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router();
-
+const {User} = require('./models/users')
 const PagesController = require('./controllers/PagesController')
 
 router.get('/', PagesController.index);
+router.get('/signup', async (req,res) => {
+    const user = new User({
+        email : 'paramvir109@gmail.com',
+        password: '12344324'
+    })
+    await user.save();
+    res.json(user);
 
+})
 router.get('/vendor/dashboard', (req, res) => {
     res.send('Vendor page');
 })
